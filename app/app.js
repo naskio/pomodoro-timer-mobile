d7a10de42e9880dde87c227c17b2fb5e413530dd
Mehdi Nassim KHODJA <khodjamehdinassim@gmail.com>
2019-08-13 14:05:57 +0100

    fix Scroll to End

--- app/app.js
+++ app/app.js
@@ -3,24 +3,29 @@ import {
     SafeAreaView,
     ScrollView,
     StatusBar,
-    Dimensions
 } from 'react-native';
 
 import Home from './views/home';
 
 import theme from "./styles/theme";
 
-const App = () => {
-    return (
-        <Fragment>
-            <StatusBar backgroundColor={theme.COLORS.PRIMARY} barStyle="dark-content"/>
-            <SafeAreaView>
-                <ScrollView contentInsetAdjustmentBehavior="automatic">
-                    <Home/>
-                </ScrollView>
-            </SafeAreaView>
-        </Fragment>
-    );
-};
+class App extends React.Component {
+    render() {
+        return (
+            <Fragment>
+                <StatusBar backgroundColor={theme.COLORS.PRIMARY} barStyle="dark-content"/>
+                <SafeAreaView>
+                    <ScrollView contentInsetAdjustmentBehavior="automatic"
+                                ref={ref => this.scrollView = ref}
+                                onContentSizeChange={() => {
+                                    this.scrollView.scrollToEnd({animated: true});
+                                }}>
+                        <Home/>
+                    </ScrollView>
+                </SafeAreaView>
+            </Fragment>
+        );
+    }
+}
 
 export default App;
